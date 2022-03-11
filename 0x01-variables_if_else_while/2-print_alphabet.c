a7166569f6da086e86d2dcdc2e498a5d1a4ab6b6
baye94 <fallcheikh893@gmail.com>
2022-03-11 17:06:43 +0000

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,10 +5,11 @@
 *Return : 0
 */
 int main(void){
-char letter = 'a' ;
-while(letter <= 'z'){
-putchar(letter);
-letter ++;
+char letter = 'a';
+while(letter <= 'z')
+{
+  putchar(letter);
+  letter++;
 }
 putchar('\n');
 return(0);
