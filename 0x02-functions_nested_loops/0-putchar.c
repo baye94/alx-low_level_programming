fc37e720e06a7dc3931f0e82af31cf34e288e095
baye94 <fallcheikh893@gmail.com>
2022-03-15 14:23:42 +0000

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,21 @@
 #include "main.h"
 
 /**
- * main - -putchar
- * @void: Empty parameter list for main.
- *
- * Description: Prints _putchar  w/out stdio
- *
- * Return: 0 for success
-*/
+ *main - entry point
+ *Return: 0
+ */
 
 int main(void)
 
 {
-  _putchar(‘_’);
-  _putchar(‘p’);
-  _putchar(‘u’);
-  _putchar(’t’);
-  _putchar(‘c’);
-  _putchar(‘h’);
-  _putchar(‘a’);
-  _putchar(‘r);
-  _putchar('\n');
-  return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *_putcar - writes char c to stdout
+ *Return: 1 on sucess
  */
+
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+  return (write(1, &c,1));
 }
