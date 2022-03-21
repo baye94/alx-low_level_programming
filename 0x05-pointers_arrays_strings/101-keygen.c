38d0e154ecf18386bd5212565e7b1931331559d0
baye94 <fallcheikh893@gmail.com>
2022-03-21 20:08:54 +0000

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,8 +10,8 @@
 
 int main(void)
 {
+    int index;
 	srand((unsigned int)(time(NULL)));   
-	int index;
 	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
 	for(index = 0; index < 12; index++)
 	{
