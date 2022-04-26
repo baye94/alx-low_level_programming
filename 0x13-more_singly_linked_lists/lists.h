af2482de52c7ca0bea58b337560f3b896b7eb957
baye94 <fallcheikh893@gmail.com>
2022-04-26 00:26:47 +0000

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -31,7 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t *head)
-listint_t *_check_and_find(listint_t *head, listint_safe *prev)
+listint_t *find_listint_loop(listint_t *head);
+
 
 #endif
