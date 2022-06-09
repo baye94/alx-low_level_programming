d3dc3353cf58d1090eb011dac4b3f8bb3cd3ac81
baye94 <fallcheikh893@gmail.com>
2022-06-09 14:33:49 +0000

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,41 @@
 #include "lists.h"
 #include <stdlib.h>
+
 /**
- * insert_dnodeint_at_index - the function
+ * delete_dnodeint_at_index - deletes a node at a specific index
+ * @head: double pointer to the linked list
+ * @index: index at which to delete node
  *
- * @h: the pointer
- * @idx: the node wanted
- * @n: data
- * Return: new node
+ * Return: 1 on success, -1 on failure
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+ 
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	/* allow memory*/
-	dlistint_t *prev, *new_node = new_node;
-	unsigned int i = 1;
+	dlistint_t *current;
+	unsigned int i;
 
-	new_node = malloc(sizeof(dlistint_t));
-
-	/* set prev head*/
-	prev = (*h);
-	/* in case of fail*/
-	if (new_node == NULL)
+	if (head == NULL || *head == NULL)
+		return (-1);
+	current = *head;
+	if (index == 0)
 	{
-		return (NULL);
-	}
-	if ((*h) == NULL)
-	{
-		return (NULL);
-	}
-	/* set next of newNode*/
-	while (i <= idx)
-	{
-		if (i == idx)
+		*head = current->next;
+		if (current->next != NULL)
 		{
-			new_node->n = n;
-			new_node->next = prev->next;
-			prev->next = new_node;
-			new_node->prev = prev;
-			new_node->next->prev = new_node;
-			return (new_node);
+			current->next->prev = NULL;
 		}
-		prev = prev->next;
-		i++;
+		free(current);
+		return (1);
 	}
-	return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		if (current->next == NULL)
+			return (-1);
+		current = current->next;
+	}
+	current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	free(current);
+	return (1);
 }
