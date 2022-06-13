f765160ca498ab3dd15e99b6cb248dd0a1047f91
baye94 <fallcheikh893@gmail.com>
2022-06-13 14:32:05 +0000

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://github.com/baye94/alx-low_level_programming/blob/master/0x18-dynamic_libraries/inject.so -P ../
-export LD_PRELOAD=../inject.so
+export LD_PRELOAD=./inject.so
